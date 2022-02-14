e0425133189e07fbc8a9d6c93298965e1d12e9fd
Rabi-Mezan <ravimezan@gmail.com>
2022-02-14 16:25:42 +0600

    order api done

--- index.js
+++ index.js
@@ -21,6 +21,37 @@ async function run() {
     try {
         client.connect();
 
+        const database = client.db('speeddo')
+        const bikeCollection = database.collection('bikes');
+        const ordersCollection = database.collection('orders')
+
+
+        // get bikes a[i]
+        app.get('/bikes', async (req, res) => {
+            const result = await bikeCollection.find({}).toArray()
+            res.send(result)
+        })
+
+        // get bike details api
+
+        app.get('/details/:id', async (req, res) => {
+
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await bikeCollection.findOne(query)
+            res.send(result)
+        })
+
+        // orders post api
+        app.post('/orders', async (req, res) => {
+            const data = req.body;
+            const result = await ordersCollection.insertOne(data)
+            console.log(result);
+            res.send(result)
+
+        })
+
+
     }
     finally {
 
